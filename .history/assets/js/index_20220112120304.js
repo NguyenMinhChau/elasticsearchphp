6ca63e2b11d42f41911d0c008f32ee5a7b3050bb
Nguyễn Minh Châu <81848005+NguyenMinhChau@users.noreply.github.com>
2022-01-12 12:05:33 +0700

    Elasticsearch server

--- .history/assets/js/index_20220112120304.js
+++ .history/assets/js/index_20220112120304.js
@@ -1,20 +1,5 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
-function datetime(){
-    var today = new Date();
-    var day = today.getDate();
-    var month = today.getMonth() + 1;
-    var year = today.getFullYear();
-    if (day < 10) {
-        day = '0' + day;
-    }
-    if (month < 10) {
-        month = '0' + month;
-    }
-    var result = day + '/' + month + '/' + year;
-    $('.datetime').innerText = result;
-}
-datetime();
 var day = new Date();
 var year = day.getFullYear();
 $$(".year").forEach(function(element) {
