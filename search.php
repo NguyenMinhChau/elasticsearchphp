86fe6ee3419305372f5ad5562b6028b9fbf58ea0
Nguyễn Minh Châu <81848005+NguyenMinhChau@users.noreply.github.com>
2022-01-19 10:39:13 +0700

    Elasticsearch php

--- search.php
+++ search.php
@@ -1,19 +1,59 @@
 <?php
     use Elasticsearch\ClientBuilder;
     require "vendor/autoload.php";
-    $hosts = [
-        [
-            'host' => 'localhost',
-            'port' => 9299,
-            'scheme' => 'http',
-        ]
-    ];
+    //kiểm tra trạng thái URL bằng curl https
+    $url9299 = 'http://localhost:9299';
+    $url9200 = 'http://localhost:9200';
+    $curl9299 = curl_init($url9299);
+    $curl9200 = curl_init($url9200);
+    curl_setopt($curl9299, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl9299, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl9200, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl9200, CURLOPT_SSL_VERIFYPEER, false);
+    $response9299 = curl_exec($curl9299);
+    $response9200 = curl_exec($curl9200);
+    $status9299 = curl_getinfo($curl9299, CURLINFO_HTTP_CODE);
+    $status9200 = curl_getinfo($curl9200, CURLINFO_HTTP_CODE);
+    curl_close($curl9299);
+    curl_close($curl9200);
+    if($status9299 != 200){
+        $hosts = [
+            [
+                'host' => 'localhost',
+                'port' => 9200,
+                'scheme' => 'http',
+            ]
+        ];
+    }
+    if($status9200 != 200){
+        $hosts = [
+            [
+                'host' => 'localhost',
+                'port' => 9299,
+                'scheme' => 'http',
+            ]
+        ];
+    }
+    if($status9299 == 200 && $status9200 == 200){
+        $hosts = [
+            [
+                'host' => 'localhost',
+                'port' => 9200,
+                'scheme' => 'http',
+            ]
+        ];
+    }
+    if($status9299 != 200 && $status9200 != 200){
+        echo '<h3>Error: Server chưa được kích hoạt. Không thể kết nối đến Elasticsearch</h3>';
+        exit();
+    }
     $client = ClientBuilder::create()->setHosts($hosts)->build();
     // kiểm tra index có tồn tại hay không
     $exists = $client->indices()->exists(['index' => 'healthcare_new']);
 
     if(!$exists){
-        throw new Exception('Chỉ mục healthcare_new không tồn tại');
+        echo '<h5>Error: Chỉ mục healthcare_new không tồn tại</h3>';
+        exit();
     }
     $search = $_POST['search'] ?? null;
     if($search != ''){
