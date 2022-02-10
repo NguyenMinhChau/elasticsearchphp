95fc21f74043d400b4020a89d83806dcd3996a66
Nguyễn Minh Châu <81848005+NguyenMinhChau@users.noreply.github.com>
2022-02-10 15:53:31 +0700

    v.1.0

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    require_once './index.php';
+    require_once './index.html';
     $page = $_GET['page'] ?? '';
     $menuitems = [
         'managerindex' => '<i class="bx bx-list-check"></i> Quản lý chỉ mục elasticsearch',
